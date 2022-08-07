3f3667f22316f0f53350162fd30f93d084cb9510
Sidhant <sidhant1593@gmail.com>
2022-08-08 03:53:38 +0530

    header started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Sidhant Kumar Sahoo</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
